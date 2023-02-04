dc2d6bbfebe1e81db5781a0a5dff9354baee88ba
Amy Snarr <amy.snarr16@gmail.com>
2023-02-03 20:46:31 -0700

    can get main.handlebars to work

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.MYSQL_PASSWORD,
     {
       host: '127.0.0.1',
       dialect: 'mysql',
