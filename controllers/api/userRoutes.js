46fd68a5a5e1ddf1f3a999c63c412955fba885c6
Amy Snarr <amy.snarr16@gmail.com>
2023-02-06 19:15:27 -0700

    can signnup and login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,37 +3,38 @@ const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const dbUserData = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+      res.status(200).json(dbUserData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
 router.post('/login', async (req, res) => {
     try {
       const dbUserData = await User.findOne({ where: { username: req.body.username } });
-
       if (!dbUserData) {
         res
           .status(400).json({ message: 'Incorrect username, please try again' });
         return;
       }
-
-      const validPassword = await dbUserData.checkPassword(req.body.password);
+      const validPassword = dbUserData.checkPassword(req.body.password);
 
       if (!validPassword) {
         res
           .status(400).json({ message: 'Incorrect password, please try again' });
         return;
-      }
 
+      }
       req.session.save(() => {
         req.session.logged_in = true;
 
@@ -41,7 +42,6 @@ router.post('/login', async (req, res) => {
       });
 
     } catch (err) {
-      console.log(err);
       res.status(400).json(err);
     }
   });
