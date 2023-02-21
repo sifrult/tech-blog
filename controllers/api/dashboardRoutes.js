522d1a3b6323befab9a067fd6ed4b9d0fd44c72c
Amy Snarr <amy.snarr16@gmail.com>
2023-02-21 11:49:41 -0700

    user can add blogposts and they appear on the dashboarD

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -2,4 +2,17 @@ const router = require('express').Router();
 const { Blogpost, User, Comment} = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.post('/newPost', withAuth, async (req, res) => {
+  try {
+    const newPost = await Blogpost.create({
+      ...req.body,
+      user_id: req.session.user_id
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err)
+  }
+})
+
 module.exports = router;
--- controllers/dashRoutes.js
+++ controllers/dashRoutes.js
@@ -4,16 +4,27 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try{
-        const blogpostData = await Blogpost.findAll({where: {user_id: req.session.user_id}})
+        const blogpostData = await Blogpost.findAll({
+          where: {user_id: req.session.user_id},
+          include: [{model: User, attributes: ['username']}]
+        });
         const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain : true }));
         console.log(blogposts)
       res.render('dashboard', {
-        ...blogposts,
+        blogposts,
         logged_in: req.session.logged_in
       });
     } catch (err) {
       res.status(500).json(err);
     }
+});
+
+router.get('/addPost', async (req, res) => {
+  try {
+    res.render('addPost');
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
