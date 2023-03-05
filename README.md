106a09a4130879c29da14f3064ca3c64381ad8ee
Amy Snarr <amy.snarr16@gmail.com>
2023-03-04 21:14:27 -0700

    changed db process.env

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# tech-blog
+# Tech Blog
+
+## Name
+Tech Blog
+
+## Description
+This site functions as a small tech blog, where you can see blog posts and comments by other users. You are able to view dates, delete blogs, and edit blogs by either title or content, or both. This site is mobile-friendly. To view this site, clikc here.
+
+## Visuals
--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,13 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.MYSQL_PASSWORD,
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
     {
-      host: '127.0.0.1',
+      host: process.env.MYSQLHOST,
       dialect: 'mysql',
-      port: 3306
+      port: process.env.MYSQLPORT
     }
   );
 }
